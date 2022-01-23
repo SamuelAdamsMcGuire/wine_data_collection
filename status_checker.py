011e28c66c0e598fe5e6f288f987d0d79cba4c38
samuel <samuel@spiced-academy.com>
2022-01-23 22:33:51 +0100

    add status checker linter

--- status_checker.py
+++ status_checker.py
@@ -1,3 +1,6 @@
+'''
+Used to see how many webpages have been scraped and how many are left
+'''
 import pickle
 from samssimplescraper import Scraper
 
@@ -5,7 +8,7 @@ from samssimplescraper import Scraper
 ROOT_URL = "https://www.winemag.com/buying-guide/"
 
 with open('./data/pickled_lists/total_links_list.pkl', 'rb') as fpick:
-        links = pickle.load(fpick)
+    links = pickle.load(fpick)
 
 scraper = Scraper(link_list=links, root_url=ROOT_URL, folders=True)
 
