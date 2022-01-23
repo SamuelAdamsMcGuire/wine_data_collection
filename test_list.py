ef3ecae44acd0e7765bf9b99679533948f151896
samuel <samuel@spiced-academy.com>
2022-01-23 22:21:42 +0100

    test

--- test_list.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-
-def test_list():
-    with open('./data/pickled_lists/total_links_list.pkl', 'rb') as fp:
-        links = pickle.load(fp)
-    assert all(isinstance(s, str) for s in links)
